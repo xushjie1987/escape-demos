7b54b56e5157c29677841fb2780cfc61802d8589
xushjie1987 <xsj2013@foxmail.com>
2016-09-10 11:55:38 +0800

    x220i

--- escape1/src/main/java/com/oneapm/escape1/TestEscape.java
+++ escape1/src/main/java/com/oneapm/escape1/TestEscape.java
@@ -32,6 +32,16 @@ public class TestEscape {
     
     /**
      * main: <br/>
+     * 30–37: Set text color (foreground) 30 + n, where n is from the color table below <br>
+     * 38: Reserved for extended set foreground color typical supported next arguments are 5;n where
+     * n is color index (0..255) or 2;r;g;b where r,g,b are red, green and blue color channels (out
+     * of 255) <br>
+     * 39: Default text color (foreground) implementation defined (according to standard) <br>
+     * 40–47: Set background color 40 + n, where n is from the color table below <br>
+     * 48: Reserved for extended set background color typical supported next arguments are 5;n where
+     * n is color index (0..255) or 2;r;g;b where r,g,b are red, green and blue color channels (out
+     * of 255) <br>
+     * 49: Default background color implementation defined (according to standard) <br>
      * 
      * @author xushjie
      * @param args
@@ -41,6 +51,22 @@ public class TestEscape {
         System.out.println(ANSI_RED +
                            "This text is red!" +
                            ANSI_RESET);
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("\u001B[11mHello World\u001B[0m");
+        System.out.println("\u001B[12mHello World\u001B[0m");
+        System.out.println("\u001B[13mHello World\u001B[0m");
+        System.out.println("\u001B[14mHello World\u001B[0m");
+        System.out.println("\u001B[15mHello World\u001B[0m");
+        System.out.println("\u001B[16mHello World\u001B[0m");
+        System.out.println("\u001B[17mHello World\u001B[0m");
+        System.out.println("\u001B[18mHello World\u001B[0m");
+        System.out.println("\u001B[19mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("\u001B[4mHello World\u001B[0m");
+        System.out.println("\u001B[21mHello World\u001B[0m");
+        System.out.println("\u001B[24mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        // 30–37 Set text color (foreground) 30 + n, where n is from the color table below
         System.out.println("\u001B[30mHello World\u001B[0m");
         System.out.println("\u001B[31mHello World\u001B[0m");
         System.out.println("\u001B[32mHello World\u001B[0m");
@@ -49,6 +75,22 @@ public class TestEscape {
         System.out.println("\u001B[35mHello World\u001B[0m");
         System.out.println("\u001B[36mHello World\u001B[0m");
         System.out.println("\u001B[37mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        // 40–47 Set background color 40 + n, where n is from the color table below
+        System.out.println("\u001B[40mHello World\u001B[0m");
+        System.out.println("\u001B[41mHello World\u001B[0m");
+        System.out.println("\u001B[42mHello World\u001B[0m");
+        System.out.println("\u001B[43mHello World\u001B[0m");
+        System.out.println("\u001B[44mHello World\u001B[0m");
+        System.out.println("\u001B[45mHello World\u001B[0m");
+        System.out.println("\u001B[46mHello World\u001B[0m");
+        System.out.println("\u001B[47mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        // Examples: to get black letters on white background use ESC[30;47m,
+        // to get red use ESC[31m,
+        // to get bright red use ESC[31;1m.
+        // To reset colors to their defaults, use ESC[39;49m (not supported on some terminals),
+        // or reset all attributes with ESC[0m
         System.out.println("\u001B[30;40mHello World\u001B[0m");
         System.out.println("\u001B[30;41mHello World\u001B[0m");
         System.out.println("\u001B[30;42mHello World\u001B[0m");
@@ -57,14 +99,17 @@ public class TestEscape {
         System.out.println("\u001B[30;45mHello World\u001B[0m");
         System.out.println("\u001B[30;46mHello World\u001B[0m");
         System.out.println("\u001B[30;47mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        // ESC[38;2;<r>;<g>;<b>m
+        // ESC[48;2;<r>;<g>;<b>m
         System.out.println("\u001B[38;2;152;118;170mHello World\u001B[0m");
         System.out.println("\u001B[38;2;152;118;170;48;2;43;43;43mHello World\u001B[0m");
-        System.out.println("\u001B[38;1;152;118;170mHello World\u001B[0m");
-        System.out.println("\u001B[38;1;152;118;170;48;1;43;43;43mHello World\u001B[0m");
-        System.out.println("\u001B[38;4;152;118;170mHello World\u001B[0m");
-        System.out.println("\u001B[38;4;152;118;170;48;4;43;43;43mHello World\u001B[0m");
-        System.out.println("\u001B[38;5;152;118;170mHello World\u001B[0m");
-        System.out.println("\u001B[38;5;152;118;170;48;5;43;43;43mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        // ESC[38;5;<n>m
+        // ESC[48;5;<n>m
+        System.out.println("\u001B[38;5;152mHello World\u001B[0m");
+        System.out.println("\u001B[38;5;152;48;5;43mHello World\u001B[0m");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
     }
     
 }
